3a81facd7ffd900175f1a5ec1f22911aef21020a
taiwola <seunolanitori@gmail.com>
2026-03-29 05:35:42 -0700

    update project routes

--- src/route/project.route.ts
+++ src/route/project.route.ts
@@ -3,6 +3,7 @@ import { createProject, deleteProject, getAllProjects, getProjectById, updatePro
 import { protect } from "../middleware/auth.middleware";
 import { createProjectValidation, updateProjectValidation } from "../validate/project.validate";
 import { validate } from "../common/validate";
+import { uploadProfileFiles } from "../config/multer.config";
 
 const router = express.Router();    
 
@@ -10,8 +11,8 @@ router.get('/', getAllProjects)
 router.get('/:id', getProjectById)
 
 
-router.post('/',  protect, createProjectValidation, validate, createProject)
-router.patch('/:id', protect, updateProjectValidation, validate, updateProject)
+router.post('/',  protect, createProjectValidation, validate, uploadProfileFiles, createProject)
+router.patch('/:id', protect, updateProjectValidation, validate, uploadProfileFiles, updateProject)
 
 router.delete('/:id', protect, deleteProject)
 
